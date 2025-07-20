98bcfa014591e4e7eab7c808e35f34beefdb7ed3
Mantas An <mantasantanavic@gmail.com>
2025-07-20 22:16:15 +0300

    Added class Category kadangi musu kategorijos tures ir subkategorijas, reikejo paturbint klase su get full path.

--- shop/models.py
+++ shop/models.py
@@ -16,7 +16,7 @@ class CustomUser (AbstractUser):
     email = models.EmailField(unique=True, blank=False)
     phone = models.CharField(max_length=15, unique=True, help_text="+370xxxxxxxx arba 0xxxxxxxx")
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
-    city = models.CharField(max_length=50, blank=True, help_text="Gyvenamoji vieta")
+    city = models.CharField(max_length=50, blank=True, help_text="Gyvenamasis miestas")
     ACCOUNT_TYPE_CHOICES = [
         ('Privatus asmuo', '(parduodu tik savo asmeninius daiktus)'),
         ('Įmonė', '(vykdau komercinę veiklą)'),
@@ -27,3 +27,30 @@ class CustomUser (AbstractUser):
         verbose_name='Paskyros tipas',
         help_text='Ar vartotojas yra privatus asmuo ar įmonė?'
     )
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Pavadinimas', max_length=50)
+    parent = models.ForeignKey(
+        to= 'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name='Tevine kategorija'
+    )
+    slug = models.SlugField(unique=True) #URL parodomas ne id, o pavadinimas.
+    order = models.PositiveIntegerField(default=0) #Rikiavimas
+
+
+    def __str__(self):
+        return self.name
+
+
+    def get_full_path(self): #Sitas visas pasiskolintas is AI, reikes issiaiskinti kaip veikia
+        #Grąžina pilnąkategorijos kelią(pvz.: 'Transportas/Automobiliai'
+        path = [self.name]
+        current = self.parent
+        while current:
+            path.append(current.name)
+            current = current.parent
+        return '/'.join(reversed(path))
